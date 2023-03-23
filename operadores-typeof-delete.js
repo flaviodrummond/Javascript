e40ec6664335ac3d9eecb16b67adaa658ccbdbc1
flaviodrummond <flaviosax20@gmail.com>
2023-03-23 11:44:12 -0300

    Operadores Typeof e Delete

---
+++ operadores-typeof-delete.js
@@ -0,0 +1,22 @@
+/* Operadores Typeof e Delete */
+
+const myNumber = 15
+const myString = "Olá Mundo"
+const myObject = {
+    name: "Flavio",
+    age: 38,
+    height: 1.69,
+}
+
+console.log(typeof myObject)
+
+
+const objectOne = {
+    name: "Rosiane",
+    age: 37,
+    height: 1.65
+}
+
+delete objectOne.height
+
+console.log(objectOne)
