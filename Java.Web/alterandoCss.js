dc42c8c2a3c147e2198769d7d6dce967b45553ec
flaviodrummond <flaviosax20@gmail.com>
2023-06-02 12:17:27 -0300

    Aprendendo alterar texto e editar o CSS pelo Javacript

---
+++ Java.Web/alterandoCss.js
@@ -0,0 +1,12 @@
+/* Alterando o CSS pelo Javacript */
+
+let alterandoCss = document.querySelector("h1")
+
+alterandoCss.style.fontSize = "50px"
+alterandoCss.style.color = "#198"
+
+let button = document.querySelector(".main-button")
+
+button.style.color = "white"
+button.style.backgroundColor = "#842132"
+button.style.fontSize = "14px"
---
+++ Java.Web/alterandoTexto.js
@@ -0,0 +1,14 @@
+/* Alterando e acessando texto
+
+textContent - Pega todo o conteúdo
+innerText - Pega apenas o texto
+innerHTML - Permite adicionar HTML e texto */
+
+let alterandoText = document.querySelector(".paragraph-js")
+alterandoText.innerHTML = "Alterando texto com o <b>javacript</b>"
+// alterandoText.textContent = "Feliz em aprender Javacript" - apenas altera o texto HTML no Javacript
+// alterandoText.innerText = "Tudo que trás no CSS" - Me trás tudo que o CSS me apresenta no texto se estiver estilizado.
+
+console.log(alterandoText.textContent) // Só o HTML
+console.log(alterandoText.innerText) // Leva em conta o CSS
+console.log(alterandoText.innerHTML) // Trás tudo e permite adicionar o HTML no meio dele
