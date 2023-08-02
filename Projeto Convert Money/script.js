e4ac85519e6c353a39bd19bd1ce3325a5f674bf5
flaviodrummond <flaviosax20@gmail.com>
2023-08-02 00:26:33 -0300

    Adicionando a primeira parte do Javascript

---
+++ Projeto Convert Money/script.js
@@ -0,0 +1,12 @@
+const convertButton = document.querySelector('.convert-button')
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    const dolarToday = 4.79
+    const convertedValue = inputCurrencyValue / dolarToday
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
+
