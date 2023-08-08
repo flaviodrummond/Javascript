c884370250a92519cabab3630a0c1977c361681a
flaviodrummond <flaviosax20@gmail.com>
2023-08-07 23:41:04 -0300

    Ajustando as conversões das moedas

--- Projeto Convert Money/script.js
+++ Projeto Convert Money/script.js
@@ -1,12 +1,32 @@
 const convertButton = document.querySelector('.convert-button')
+const currencySelect = document.querySelector(".currency-select-to-convert")
 
-function convertValues(){
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToCovert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
 
-    const dolarToday = 4.79
-    const convertedValue = inputCurrencyValue / dolarToday
-    console.log(convertedValue)
+    const dolarToday = 4.90
+    const euroToday = 5.39
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-us", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-De", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    currencyValueToCovert.innerHTML = new Intl.NumberFormat("pt-br", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
 }
 
 convertButton.addEventListener("click", convertValues)
-
