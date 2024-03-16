27b41a03cdb97e454e8bb064548e69b3ed9ce33e
flaviodrummond <flaviosax20@gmail.com>
2024-03-16 12:45:15 -0300

    revendo o inerHTML e aprendendo sobre formatação de valores

--- Projeto Convert Money/script.js
+++ Projeto Convert Money/script.js
@@ -1,13 +1,21 @@
 const convertButton = document.querySelector('.convert-button')
 
 function convertValues(){
+
     const inputCurrencyValue = document.querySelector('.input-currency').value
+
+    const currencyValueToConvert = document.querySelector('.currency-value-to-convert')
+
+    const currencyValueToConverted = document.querySelector('.currency-value')
     
     const dollarToday = 4.98
     
     const convertedValue = inputCurrencyValue / dollarToday
 
-    console.log(convertedValue)
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(inputCurrencyValue);
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(convertedValue);
+
 }
 
 convertButton.addEventListener("click", convertValues)
