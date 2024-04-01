682fbfb547bfb3bf9637a1899d09367c8fdd96d5
flaviodrummond <flaviosax20@gmail.com>
2024-04-01 20:38:40 -0300

    revisando operadores de atribuição

--- operadores-atribuição.js
+++ operadores-atribuição.js
@@ -17,5 +17,9 @@ result += 60
 console.log(result) // Está adição é a mesma do que (result = result + 60). O que muda apenas é economizar código.
 
 let myNumber = 15
-myNumber **=2 // As invés de fazer a variável duas vezes, posso simplificar usando as atribuições
+myNumber **=3 // As invés de fazer a variável duas vezes, posso simplificar usando as atribuições
 console.log(myNumber)
+
+let oneNumber = 40
+oneNumber %= 3
+console.log(oneNumber)
