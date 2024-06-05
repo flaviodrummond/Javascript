535c5e135d14104cb90ac821ff990a50f3821cf9
flaviodrummond <flaviosax20@gmail.com>
2024-06-05 11:25:26 -0300

    Revendo novos conhecimentos

--- array.js
+++ array.js
@@ -3,3 +3,17 @@ const numbers = [10, "Vasco da Gama", 1984, "Ser Milionário"]
 console.log(numbers [3]) // o [3] é para aparecer na tela o que eu quero que apareça dentro da variável.
 console.log(numbers)
 /* O primeiro array, sempre começa na posição zero */
+
+const user = [{
+    name: "Flávio",
+    age: 39,
+    city: "Vila Velha"
+}, 
+{
+    name: "Rosiane",
+    age: 38,
+    city: "Vila Velha"  
+}]
+
+user[1].age = 39 /* Alterando algum dado dentro do objeto */
+console.log(user)
